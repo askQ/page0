2f9541ebfb7a5f612225ce36953639e8319e1e0d
aeifkz <aeifkz@hotmail.com>
2014-06-18 22:23:17 +0800

    add sessionid get set ,ethod

--- src/com/example/util/Tool.java
+++ src/com/example/util/Tool.java
@@ -4,6 +4,17 @@ import java.security.MessageDigest;
 
 public class Tool {
 	
+	private static String sessionid ;
+	
+	//設定 or 讀取 sessionid 用函式
+	public static String getSessionid() {
+		return sessionid;
+	}
+
+	public static void setSessionid(String sessionid) {
+		Tool.sessionid = sessionid;
+	}
+
 	//MD5 加密函式 , 回傳 16 進位字串
 	public static String md5(String str) {				
 		MessageDigest md5;
