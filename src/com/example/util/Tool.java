f63aed86b1014591c2e39536aeda200c3269d2a6
aeifkz <aeifkz@hotmail.com>
2014-06-19 11:10:43 +0800

    add translate bitmap to base64 string function

--- src/com/example/util/Tool.java
+++ src/com/example/util/Tool.java
@@ -1,6 +1,10 @@
 package com.example.util;
 
+import java.io.ByteArrayOutputStream;
 import java.security.MessageDigest;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
 
 public class Tool {
 	
@@ -14,6 +18,29 @@ public class Tool {
 	public static void setSessionid(String sessionid) {
 		Tool.sessionid = sessionid;
 	}
+	
+	//傳入Bitmap參數 , 回傳JPG格式的Base64編碼結果
+		public static String encode_to_base64(Bitmap bitmap){
+			
+			ByteArrayOutputStream bos = new ByteArrayOutputStream() ;
+	        bitmap.compress(Bitmap.CompressFormat.JPEG,100,bos) ;
+	        
+	        //get img btye array
+	        byte [] b_data = bos.toByteArray() ;
+	        
+	        //encode byte array to base64 format 
+	        byte [] new_data = Base64.encode(b_data, Base64.DEFAULT) ;
+	        
+	        StringBuilder buffer = new StringBuilder() ;
+	        
+	        //get byte data from base64 array , and save as char format 
+	        for(int i=0 ; i<new_data.length ; i++) {
+	        	buffer.append((char)new_data[i]) ;
+	        }
+	        
+	        return buffer.toString() ;
+		}
+	
 
 	//MD5 加密函式 , 回傳 16 進位字串
 	public static String md5(String str) {				
