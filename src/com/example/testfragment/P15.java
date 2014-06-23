557afb7d3e51cb7a727c7d83b9bff49c5330f4eb
vul9156 <vul9156@hotmail.com>
2014-06-23 23:12:25 +0800

    today i change the 返回見會去到的頁面

--- src/com/example/testfragment/P15.java
+++ src/com/example/testfragment/P15.java
@@ -105,6 +105,63 @@ public class P15 extends Activity {
 	  		});
 	        
 	    }
+	     public boolean onKeyDown(int keyCode,KeyEvent event){
+
+	      if(keyCode==KeyEvent.KEYCODE_BACK && event.getRepeatCount()==0){   //確定按下退出鍵and防止重複按下退出鍵
+
+	          dialog();
+
+	      }
+
+	      return false;
+
+	  }
+
+	  
+
+	   private void dialog(){
+
+	       AlertDialog.Builder builder = new AlertDialog.Builder(P15.this); //創建訊息方塊
+
+	       builder.setMessage("預覽問題不想問了嗎？");
+
+	       builder.setTitle("不問了");
+
+	       builder.setPositiveButton("確認", new DialogInterface.OnClickListener()  {
+
+	           @Override
+
+	           public void onClick(DialogInterface dialog, int which) {
+	        	   Intent intent = new Intent();
+					intent.setClass(P15.this, Page4_MainActivity.class);
+					startActivity(intent); 
+					
+	              dialog.dismiss(); //dismiss為關閉dialog,Activity還會保留dialog的狀態
+
+	              P15.this.finish();//關閉activity
+	              
+	              
+
+	      }
+
+	    });
+
+	       builder.setNegativeButton("繼續問", new DialogInterface.OnClickListener()  {
+
+	           @Override
+
+	           public void onClick(DialogInterface dialog, int which) {
+
+	        dialog.dismiss();
+
+	      }
+
+	    });
+
+	    builder.create().show();
+
+	   }
+	
 	
 	
 }
